e1fca763ef6819b00b28df175be154d71ee8d942
stephanie <o1083029@gm.pu.edu.tw>
2021-03-15 10:51:08 +0800

    第二次 Commit（修改文字元件）

--- app/src/main/java/tw/edu/pu/o1083029/hello2021gege/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/o1083029/hello2021gege/MainActivity.kt
@@ -2,10 +2,14 @@ package tw.edu.pu.o1083029.hello2021gege
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var txv: TextView =findViewById<TextView>(R.id.txv)
+        txv.text = "Hello Athena"
     }
 }
